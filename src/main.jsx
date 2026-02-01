1ebe72a343fdab74b8c21dddecd4f438ebe1fa6e
stjand <surya567teja@gmail.com>
2026-02-01 12:51:52 +0530

    Fix: brute-force polyfill injection in main.jsx to guarantee process.nextTick availability

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+import { Buffer } from 'buffer';
+import process from 'process';
+
+// Polyfill globals immediately
+if (typeof window !== 'undefined') {
+    window.global = window;
+    window.process = process;
+    window.Buffer = Buffer;
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
